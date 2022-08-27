a0cbb3b95917fd14c86ea3aaadb5efe27030a04f
Renzo Moises Piguave Cevallos <renzomoisespiguave@renzos-mbp.home>
2022-08-27 01:23:38 +0100

    make it better

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
-var http = require('http');
-var fs = require('fs');
+// var http = require('http');
+// var fs = require('fs');
 
-const PORT=8080; 
+// const PORT=8080; 
 
-fs.readFile('./index.js', function (err, html) {
+// fs.readFile('./index.js', function (err, html) {
 
-    if (err) throw err;    
+//     if (err) throw err;    
 
-    http.createServer(function(request, response) {  
-        response.writeHeader(200, {"Content-Type": "text/html"});  
-        response.write(html);  
-        response.end();  
-    }).listen(PORT);
+//     http.createServer(function(request, response) {  
+//         response.writeHeader(200, {"Content-Type": "text/html"});  
+//         response.write(html);  
+//         response.end();  
+//     }).listen(PORT);
 
-    console.log("Server js called")
-});
+//     console.log("Server js called")
+// });
